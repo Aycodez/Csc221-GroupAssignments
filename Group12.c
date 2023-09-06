72ef611ebcb4eb5a47b25f169b602de8f114f6bc
ghosthunter-2 <144274536+ghosthunter-2@users.noreply.github.com>
2023-09-06 21:25:11 +0200

    Update Group12.c

--- Group12.c
+++ Group12.c
@@ -1,48 +1,61 @@
-#include <stdio.h>
-#include <stdbool.h>
+// Creating a doubly linked list
+# include <stdio.h>
+# include <stdlib.h>
 
-#define MAX_SIZE 100
+// Creating a new data structure for the nodes 
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
 
-int stack[MAX_SIZE];
-int top = -1;
+int main()
+{
+    // initializing the head of the link list and pointer to show where we are in the linked list
+    struct Node *head = NULL;
+    int num_nodes;
+    printf("Enter number of nodes you want to link: ");
+    scanf("%i",&num_nodes);
+    struct Node *cpt = NULL;
+    // using a for loop to link the nodes and insert element into them
+    for (int i = 0;i < num_nodes; i ++)
+    {
+        // Creating a new node
+        struct Node *newnode = malloc(sizeof(struct Node));
+        // getting  the user inputand inserting into the data part of the node
+        int element;
+        printf("Enter a number to enter into node %i: ",i + 1);
+        scanf("%i",&element);
+        newnode -> data  = element;
 
-//Function to check if the stack is empty
-bool isEmpty() {
-    return top == -1;
-}
-bool isFull(){
-    return top == MAX_SIZE -1;
-}
-
-void push(int item) {
-    if(isFull()){
-        printf("Stack Overflow! Cannot push element.\n");
-    }else{
-        top++;
-        stack[top]=item;
-        printf("Pushed %d into the stack.\n",item);
-    }
-}
-
-    int pop() {
-        if (isEmpty()){
-            printf("Stack Underflow! Cannot pop element.\n");
-            return -1; //Return an error value (you can choose a different approach)
-        } else{
-            int item = stack [top];
-            top--;
-            return item;
+        // if this is the first node it becomes the head and pointer is set to it
+        if (head == NULL)
+        {
+            head = newnode;
+            head ->prev = NULL;
+            head -> next = NULL;
+            cpt = head;
         }
-}
+        else
+        {
+            cpt -> next = newnode;
+            newnode ->prev = cpt;
+            newnode -> next = NULL;
+            cpt = newnode;
+        
+        }
+        
 
-int main() {
-        push(10);
-        push(20);
-        push(30);
+    }
+    printf("Doubly linked list created successfully");
+    // Freeing up the space allocated to each nodes
+    struct Node *current = head;
+    while (current != NULL)
+    {
+        struct Node *temp = current ->next;
+        free(current);
+        current = temp;
+    }
 
-        printf("Popped %d from the stack.\n", pop());
-        printf("Popped %d from the stack.\n", pop());
-        printf("Popped %d from the stack.\n", pop());
-
-    return 0;
 }
